3f092e67b6c21bc1ff9681dd86fd35abae31da97
David Seibert <david@seibert.io>
2021-02-01 19:01:04 +0100

    fix: custom project release config is required twice

--- src/base/semantic-release.ts
+++ src/base/semantic-release.ts
@@ -4,10 +4,11 @@ import * as appRoot from 'app-root-path';
 import * as semanticRelease from 'semantic-release';
 
 function getSemanticReleaseOptions(): semanticRelease.Options {
+	// eslint-disable-next-line global-require,import/no-dynamic-require
 	const baseOptions: semanticRelease.Options = require(path.resolve(__dirname, '../../release.config.base.js')); // eslint-disable-line @typescript-eslint/no-var-requires
 	const localOptionsFilename: string = `${appRoot}/release.config.js`;
 
-	return fs.existsSync(localOptionsFilename) ? require(localOptionsFilename) : baseOptions;
+	return fs.existsSync(localOptionsFilename) ? {} : baseOptions;
 }
 
 export { getSemanticReleaseOptions };
